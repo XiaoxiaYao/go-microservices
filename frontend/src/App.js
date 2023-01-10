2a80ae887b30c1f0a4f596005df7b5b5339aff73
MichaelYao <michaelyaonz@gmail.com>
2023-01-10 15:33:47 +0800

    Create home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,33 @@
-import Button from '@mui/material/Button';
+import {
+  Button,
+  Typography,
+  CssBaseline,
+  Container,
+  Box,
+  Grid,
+} from '@mui/material';
 
 function App() {
   return (
-    <Button variant="contained">Hello World</Button>
+    <>
+      <CssBaseline />
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          Test microservices
+        </Typography>
+        <Box>Output shows here...</Box>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Typography variant="h6">Sent...</Typography>
+            <Box>Output shows here...</Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="h6">Received...</Typography>
+            <Box>Output shows here...</Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
